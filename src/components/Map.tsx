5b44e9da33340414e6aed3c07d40b0dc4dea5413
PrajwalKarale <prajwalkarale0508@gmail.com>
2024-11-20 17:34:28 -0800

    Updated Map.tsx file for marker icon in deployment

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,23 +2,32 @@ import Place from "../api/Place";
 import 'leaflet/dist/leaflet.css';
 import {Map as LeafletMap} from 'leaflet';
 import { useEffect, useRef } from "react";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
 
 interface MapProps{
     place: Place | null;
 };
 
-export default function Map({ place }: MapProps){
-    const mapRef = useRef<LeafletMap | null>(null);
+function FlyToLocation({ place }: { place: Place | null }) {
+    const map = useMap();
     useEffect(() => {
-        if(mapRef.current && place){
-            mapRef.current.flyTo([place.latitude, place.longitude])
+        if (place) {
+            map.flyTo([place.latitude, place.longitude], 12); // Adjust zoom if needed
         }
-    }, [place]);
+    }, [place, map]);
+    return null;
+}
+export default function Map({ place }: MapProps){
+    // const mapRef = useRef<LeafletMap | null>(null);
+    // useEffect(() => {
+    //     if(mapRef.current && place){
+    //         mapRef.current.flyTo([place.latitude, place.longitude])
+    //     }
+    // }, [place]);
 
     return (
         <MapContainer
-            ref = {mapRef}
+            // ref = {mapRef}
             center={[40.7, -74]}
             zoom={12}
             scrollWheelZoom
@@ -26,6 +35,7 @@ export default function Map({ place }: MapProps){
         >
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
             {place && <Marker position={[place.latitude, place.longitude]}/>}
+            <FlyToLocation place={place} />
         </MapContainer>
     );
 }
