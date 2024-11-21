715f946281c815d78f93c470f6dc92d220e7e33e
PrajwalKarale <prajwalkarale0508@gmail.com>
2024-11-20 19:11:32 -0800

    Updated Maps.tsx

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -13,6 +13,11 @@ const DefaultIcon = L.icon({
     iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
     iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+    iconSize: [25, 41], // Set the size of the icon (width, height)
+    iconAnchor: [12, 41], // Anchor the icon to the point on the map (bottom center)
+    popupAnchor: [1, -34], // Position of the popup relative to the icon
+    shadowSize: [41, 41], // Size of the shadow icon
+    shadowAnchor: [12, 41], // Anchor of the shadow icon
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
